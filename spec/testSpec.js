09c7bbe38cd9051c338936abedb52f49f88bc8e2
atrevisanut <atrevisanut@gmail.com>
2017-04-04 15:20:31 -0600

    js basic assessment part 2

---
+++ spec/testSpec.js
@@ -0,0 +1,11 @@
+
+describe("Dog Object", function(){
+  it("Dog's elemental power should be equal to water", function(){
+    expect(dog.elementalPower).toEqual('water');
+  });
+});
+describe("Cat Object", function(){
+  it("Cat's attitude should be calm", function(){
+    expect(cat.attitude).toEqual('calm');
+  });
+});
